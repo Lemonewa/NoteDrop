4f5433db484bc74b6353384282a938e929a9c3f5
Lemonewa <lemonewa@gmail.com>
2026-08-28 01:39:12 +0200

    slightly higher keyboard but this time good file is added

--- frontend/src/Piano_Logic.ts
+++ frontend/src/Piano_Logic.ts
@@ -1,7 +1,7 @@
 export const Preview_Width = 960
 export const Preview_Height = 720
 export const Preview_Fall_Time = 4
-export const Preview_Keyboard_Height = 100
+export const Preview_Keyboard_Height = 120
 export const Preview_Hit_Height = Preview_Height - Preview_Keyboard_Height
 export const Preview_Min_Pitch = 21
 export const Preview_Max_Pitch = 108
